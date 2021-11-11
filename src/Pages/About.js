84284e4a5e4291cbbe57903eb52aa08f963cdd1a
dunnticia633 <dunnticia633@gmail.com>
2021-11-11 16:29:31 -0500

    added to about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {
-  Container,
-  Row,
-  Col
-} from 'react-bootstrap';
+import {Container, Row, Col, Card} from 'react-bootstrap';
 
 export default function About() {
     return (
         <div>
             <Container fluid>
                 <Row>
-                    <Col>We sell fishing stuff!</Col>
+                    <Col className="m-5">
+                        <Card className="shadow-sm p-3 mb-5 bg-body rounded mx-auto" body>We sell fishing stuff!</Card>
+                    </Col>
                 </Row>
             </Container>
         </div>
